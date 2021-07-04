0fad329f9e18fa857c1f4158881286632c83426c
kpg <kpg@posteo.net>
2021-07-04 20:13:26 +0000

    set sbcl overlay to 2.1.6
    FossilOrigin-Name: 43cc46320f251191b39f46fe97eb90436e767873e2256acfe957532167bf93f9

--- dell-laptop/overlays/sbcl.nix
+++ dell-laptop/overlays/sbcl.nix
@@ -1,4 +1,4 @@
 import "${<nixos>}/pkgs/development/compilers/sbcl/common.nix" {
-  version = "2.1.5";
-  sha256 = "sha256:1id8r2y8hiz34lmvszikdqgrmr778jshim0fibb915assvn0fn4n";
+  version = "2.1.6";
+  sha256 = "sha256:1vydq6mmssla2vn0plpsrm50n3b42a464zxw72j6qihas9fhq8cb";
 }
