3573eb4746892399a44c8d76170328d000d795a7
kpg <kpg@posteo.net>
2021-09-08 23:14:05 +0000

    add note about NIX_PATH
    FossilOrigin-Name: 349d008da363ba054e28a945fb01b23fcc09ee407df7c1c363c178589f8dff25

--- nixpkgs/default.nix
+++ nixpkgs/default.nix
@@ -1,4 +1,5 @@
 builtins.fetchTarball {
+  # read from file then set NIX_PATH=https://... ?
   url    = https://github.com/NixOS/nixpkgs/archive/97c5d0cbe76901da0135b05cdbdfc5b068a7942c.tar.gz;
   sha256 = "05zisan8s08r45jbn9xc3dysnsdh639qn7jhczsd71mjfg85n13v";
 }
