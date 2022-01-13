6b97eb3e3130b158aeeb81744a54bfd2347cb4e3
kpg <kpg@posteo.net>
2022-01-13 13:13:38 +0000

    typo
    FossilOrigin-Name: 04bcdbb0dc7d0b3569f1d7168f3b8d6e5465449b3b38ea27f34374b7a513cac2

--- systems/pc/emacs/default.el
+++ systems/pc/emacs/default.el
@@ -154,7 +154,7 @@
   (indent-tabs-mode nil)
   (tab-width 4)
   (column-number-mode t)
-  (setf line-number-mode t)
+  (line-number-mode t)
   :config
   (minibuffer-depth-indicate-mode t)
   (tool-bar-mode -1)
