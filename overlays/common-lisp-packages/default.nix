2918a99334997620529039237b756a8a70f5a53e
kpg <kpg@posteo.net>
2022-02-06 20:54:42 +0000

    bump nix-cl
    FossilOrigin-Name: 0d140863d3dd07b0d57b1a9ba88c663c2780c9d3c152066cef156160b3657516

--- overlays/common-lisp-packages/default.nix
+++ overlays/common-lisp-packages/default.nix
@@ -1,6 +1,6 @@
 { pkgs, ... }:
 
 import (builtins.fetchTarball {
-  url = "https://github.com/uthar/nix-cl/archive/55c5d5af3e63f76e1080ea4a12cf62b80e1eb0a5.tar.gz";
-  sha256 = "0w35ams0ibsx8z2x8qqxbnfj5b8mb084hb6nskdh3xazrdhg7j1i";
+  url = "https://github.com/uthar/nix-cl/archive/c6c087de2802812bc9d5baa4c72955009050c1aa.tar.gz";
+  sha256 = "19hyhg55jj3w6d71l9c5xp4gik5ykjqbdbzd0y1f5xhbv68ysb0b";
 }) { inherit pkgs; }
