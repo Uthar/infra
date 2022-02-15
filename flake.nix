6cee8bc42b76c1cbb0d48d222589efe386f8861a
kpg <kpg@posteo.net>
2022-02-15 21:26:26 +0000

    configure l15-pix as flake
    FossilOrigin-Name: 058c58efd746267795c5d44f97c1f1e5bc619e85ea2bcbc8c073037a23d9a41f

--- flake.nix
+++ flake.nix
@@ -5,6 +5,7 @@
     nixpkgs-21_11.url  = "github:NixOS/nixpkgs/21.11";
     nixos-21_11.url    = "github:NixOS/nixpkgs/nixos-21.11";
     nixpkgs-master.url = "github:NixOS/nixpkgs/master";
+    nixos-hardware.url = "github:NixOS/nixos-hardware/master";
   };
 
  outputs = {
@@ -12,6 +13,7 @@
    ,nixpkgs-21_11
    ,nixos-21_11
    ,nixpkgs-master
+   ,nixos-hardware
  }: {
 
    nixosConfigurations = {
@@ -27,6 +29,25 @@
          }
          {
            nixpkgs.overlays = import ./overlays/default.nix;
+         }
+       ];
+     };
+
+     l15-pix = nixos-21_11.lib.nixosSystem {
+       system = "x86_64-linux";
+       modules = [
+         ./machines/l15-pix
+         (nixos-hardware + "/lenovo/thinkpad/l14")
+         ./systems/pc
+         {
+           # Let 'nixos-version --json' know about the Git revision of this flake.
+           system.configurationRevision = nixpkgs-21_11.lib.mkIf (self ? rev) self.rev;
+         }
+         {
+           nixpkgs.overlays = import ./overlays/default.nix;
+         }
+         {
+           boot.supportedFilesystems = nixpkgs-21_11.lib.mkForce [ "ext4" ];
          }
        ];
      };
